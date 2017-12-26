18a6c930127bd4b69c87e7b4e3b942aef2b8491c
Unknown <patrick@bryce.demon.nl>
2017-12-26 23:15:47 +0100

    Auto stash before merge of "master" and "origin/master"
    Fixed responses

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import importlib
 import discord
 import asyncio
+import json
+import random
 
 token = None
 with open('cred.txt') as f:
     token = f.read().rstrip()
 
-print(token)
+responses = None
+with open('responses.json') as f:
+    responses = json.loads(f.read())
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-        
+    try:
+        response = random.choice(responses[message.content])
+        await(client.send_message(message.channel, "```"+response+"```"))
+    except:
+        pass
 
 client.run(token)
