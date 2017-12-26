95111f0816fd4ad715553dab235305fb3081a241
Unknown <patrick@bryce.demon.nl>
2017-12-26 22:59:51 +0100

    added random choices

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import importlib
 import discord
 import asyncio
+import json
+import random
 
 token = None
 with open('cred.txt') as f:
     token = f.read().rstrip()
 
+responses = None
+with open('responses.json') as f:
+    responses = json.loads(f.read())
+
 print(token)
 
 client = discord.Client()
+debug = client.get_channel(395329131606179851)
 
 @client.event
 async def on_message(message):
     if(message.content == '!ping'):
         await client.send_message(message.channel, "pong!")
+    elif(message.content == '!help'):
+        await client.send_message(message.channel, "```Hi there!\nI'm CL4P-TP! but you can call me B4DA55 for short.\nIm here to manage you bunch.```")
+    else:
+        try:
+            response = random.choice(responses[message.content])
+            await(client.send_message(message.channel, "```"+response+"```"))
+        except:
+            pass
 
 client.run(token)
